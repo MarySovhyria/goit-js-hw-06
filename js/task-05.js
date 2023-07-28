0b6e2a923f03cdd68005019eefc266adf88a374b
MarySovhyria <111424962+MarySovhyria@users.noreply.github.com>
2023-07-28 11:29:19 +0100

    Add files via upload

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,14 @@
 const input = document.querySelector('#name-input');
 const holder = document.querySelector('#name-output');
 
-input.addEventListener("keydown", events => {
-    // input.textContent = input.textContent.trim();
-    if (holder.textContent == "Anonymous" && input != '') {
-        holder.textContent = '';
+input.addEventListener("keyup", (events) => {
+    const inputValue = input.value.trim();
+
+    if (inputValue === '') {
+      holder.textContent = 'Anonymous';
+    } else {
+      holder.textContent = inputValue;
     }
-    holder.append(events.key);
+    
 }
 )
